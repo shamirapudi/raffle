ae2ea3a9e4e54e62faf4ccdb2eae41b1994e9062
Shami Rapudi <rapudishami@gmail.com>
2021-11-12 21:34:04 +0200

    day-1(localhost,and still pushing?)

--- test.php
+++ test.php
@@ -17,7 +17,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Welcome</title>
+    <title>Welcome bro is everything working</title>
     <link href="css/bootstrap.min.css" rel="stylesheet" >
   </head>
   <body>
